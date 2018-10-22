2930abc0521a5e7b135d59a9f92b38add64bc713
barte <bartekgodon@yahoo.co.uk>
2018-10-22 13:50:36 +0100

    added admin page, search function searches on multiple columns

--- filter.js
+++ filter.js
@@ -21,23 +21,41 @@ function open(){
   activePanel.style.maxHeight = activePanel.scrollHeight + "px";
 }
 
-function searchDatabase(){
-  // Declare variables
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("filter_search");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("keyDatabase");
-  tr = table.getElementsByTagName("tr");
 
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
+// function searchDatabase(){
+//   // Declare variables
+//   var input, filter, table, tr, td, i;
+//   input = document.getElementById("filter_search");
+//   filter = input.value.toUpperCase();
+//   table = document.getElementById("keyDatabase");
+//   tr = table.getElementsByTagName("tr");
+//
+//   // Loop through all table rows, and hide those who don't match the search query
+//   for (i = 0; i < tr.length; i++) {
+//     td = tr[i].getElementsByTagName("td")[0];
+//     if (td) {
+//       if (td.innerHTML.toUpperCase().indexOf(filter) > -1 ) {
+//         tr[i].style.display = "";
+//       } else {
+//         tr[i].style.display = "none";
+//       }
+//     }
+//   }
+
+function searchDatabase() {
+    var input = document.getElementById("filter_search");
+    var filter = input.value.toUpperCase();
+    var rows = document.querySelector("#keyDatabase tbody").rows;
+
+    for (var i = 0; i < rows.length; i++) {
+        var firstCol = rows[i].cells[0].textContent.toUpperCase();
+        var secondCol = rows[i].cells[1].textContent.toUpperCase();
+        var thirdCol = rows[i].cells[2].textContent.toUpperCase();
+
+        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1 || thirdCol.indexOf(filter) > -1) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
     }
-  }
 }
