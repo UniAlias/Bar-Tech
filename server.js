512986e98f4710e42d1d2eeaedb6cfbc5e568664
Ryan McConnell <r.j.mcconnell1@rgu.ac.uk>
2018-10-25 15:57:07 +0100

    Implementing templating

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//filter Page
+app.get('/filter', function(req, res) {
+  res.render('pages/filter');
+});
+
+//account page
+app.get('/account', function(req, res) {
+  res.render('pages/account');
+});
+
+//admin page
+app.get('/admin', function(req, res) {
+  res.render('pages/admin');
+});
+
+//login page
+app.get('/login', function(req, res) {
+  res.render('pages/login');
+});
+
+
+app.listen(8080);
+console.log('8080 is the port');
