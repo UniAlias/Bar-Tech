d6315da23819d1312d989c10f7fb5561fa3d32ee
Ryan McConnell <ryanmcconnell.rm@gmail.com>
2018-10-27 16:33:04 +0100

    Creating the login system, made changes to login form.

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
-var express = require('express');
-var app = express();
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/Bar-Tech";
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser')
+const app = express();
 
+app.use(session({ secret: 'example' }));
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 
+var db;
+
+MongoClient.connect(url, function(err, database) {
+if (err) throw err;
+db = database;
+app.listen(8080);
+console.log('8080 is the port');
+});
+
+//Creating the people collection
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  db.createCollection("users", function(err, res) {
+    console.log("Collection created!");
+    db.close();
+  });
+});
+
+//Inserting Data into the 'users' Collection
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var myobj = [
+    {username: 'shona110', password: 'keymaster123', clearance: '3'},
+    {username: 'admin', password: 'uniAdmin', clearance: '3'}
+  ];
+  db.collection("users").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of users inserted: " + res.insertedCount);
+    db.close();
+  });
+});
+
+
+
+//================RENDER PAGES===============
 //filter Page
 app.get('/filter', function(req, res) {
   res.render('pages/filter');
@@ -22,10 +64,22 @@ app.get('/admin', function(req, res) {
 });
 
 //login page
-app.get('/login', function(req, res) {
+app.get('/', function(req, res) {
   res.render('pages/login');
 });
 
 
-app.listen(8080);
-console.log('8080 is the port');
+//================POST ROUTES==================
+//Login post ROUTE
+app.post('/dologin', function(req, res) {
+  console.log(JSON.stringify(req.body));
+  var uname = req.body.username;
+  var pword = req.body.password;
+
+  db.collection('users').findOne({"login.username":uname}, function(err, result) {
+    if (err) throw err;
+    if (!result){res.redirect('/login');return}
+    if(result.login.password == pword){req.session = true; req.session.currentusername = result.login.username;res.redirect('/filter')}
+    else {res.redirect('/login')}
+  });
+});
