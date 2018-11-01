89d224163696f765ba8fc5e204f0dca00505c6a5
Ryan McConnell <r.j.mcconnell1@rgu.ac.uk>
2018-11-01 14:38:55 +0000

    removed initial user add options

--- server.js
+++ server.js
@@ -32,18 +32,18 @@ MongoClient.connect(url, function(err, db) {
 });
 
 //Inserting Data into the 'users' Collection
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var myobj = [
-    {username: 'shona110', password: 'keymaster123', clearance: '3'},
-    {username: 'admin', password: 'uniAdmin', clearance: '3'}
-  ];
-  db.collection("users").insertMany(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("Number of users inserted: " + res.insertedCount);
-    db.close();
-  });
-});
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var myobj = [
+//     {username: 'shona110', password: 'keymaster123', clearance: '3'},
+//     {username: 'admin', password: 'uniAdmin', clearance: '3'}
+//   ];
+//   db.collection("users").insertMany(myobj, function(err, res) {
+//     if (err) throw err;
+//     console.log("Number of users inserted: " + res.insertedCount);
+//     db.close();
+//   });
+// });
 
 
 
